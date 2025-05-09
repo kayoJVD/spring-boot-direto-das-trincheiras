06c49a1d56b32121877b8735a907eda7dd74685e
Kayo Alves De Santana <alveskayo689@gmail.com>
2025-05-09 13:40:03 -0300

    Video18: Decoupling domain requests and responses video19: Adding MapStruct

---
+++ anime-service/src/main/java/academy/devdojo/responde/ProducerGetResponse.java
@@ -0,0 +1,16 @@
+package academy.devdojo.responde;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class ProducerGetResponse {
+    private Long id;
+    private String name;
+    private LocalDateTime creatdAt;
+}
