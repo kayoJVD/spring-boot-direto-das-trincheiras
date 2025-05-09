7dd0d80d6f75827e7b2f7166c6780c85c3e23881
Kayo Alves De Santana <alveskayo689@gmail.com>
2025-05-09 17:14:36 -0300

    Video23: Exercise DeleteMapping

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -62,4 +62,17 @@ public class AnimeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        log.debug("Request to delete anime by id: {}", id);
+        Anime animeToDelete = Anime.animeList()
+                .stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+
+        Anime.animeList().remove(animeToDelete);
+        return ResponseEntity.noContent().build();
+    }
+
 }
---
+++ exercices/Exercise05.md
@@ -0,0 +1,3 @@
+Create an endpoint 
+to delete an Anime from the 
+existing list, using @DeleMapping
